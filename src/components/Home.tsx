978dcca2721bd0daf7ab68956b0239eb4523fc38
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-08-04 18:46:58 +0300

    -

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import frontendImage from "../assets/image.png";
+
+function App() {
+  return (
+    <>
+      <img src={frontendImage} alt="" style={{ height: "100px" }} />
+      <h2>Micro frontend app</h2>
+    </>
+  );
+}
+
+export default App;
