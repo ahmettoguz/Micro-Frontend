63237f242f38cb89aa2dec5baa23650587827019
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-08-04 20:20:24 +0300

    -

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,35 @@
+import axios from "axios";
 import frontendImage from "../assets/image.png";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  const makeRequest = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts/1"
+      );
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleClick = async () => {
+    const tempData = await makeRequest();
+    setData(tempData);
+  };
+
   return (
     <>
-      <img src={frontendImage} alt="" style={{ height: "100px" }} />
-      <h2>Micro frontend app</h2>
+      <h1>Micro frontend App</h1>
+      <button onClick={handleClick}>Send Request To backend</button>
+      <br />
+      {data && <p>{data.title}</p>}
+      <br />
+      <img src={frontendImage} alt="" style={{ height: "50px" }} />
     </>
   );
 }
