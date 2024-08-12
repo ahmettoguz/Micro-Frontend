d0e28f4f3f920ab93dfeb6e9816891b636cf59a8
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-08-13 01:15:48 +0300

    env variables added for multiple components with best practice

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import viteLogo from "/vite.svg";
 
 import Home from "./components/Home";
 
+import { APP_NAME, APP_DESCRIPTION } from './utils/envVars';
+
 function App() {
   return (
     <>
       <Home></Home>
       <br />
       <br />
+      <strong>App name: </strong> <span>{APP_NAME}</span>
+      <br />
+      <strong>App description: </strong> <span>{APP_DESCRIPTION}</span>
       <br />
       <hr />
       <div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import frontendImage from "../assets/image.png";
 import { useState } from "react";
 
+import { APP_NAME, APP_DESCRIPTION } from '../utils/envVars';
+
 function App() {
   const [data, setData] = useState(null);
 
@@ -25,11 +27,17 @@ function App() {
   return (
     <>
       <h1>Micro frontend App</h1>
+      <br />
       <button onClick={handleClick}>Send Request To backend</button>
       <br />
       {data && <p>{data}</p>}
       <br />
       <img src={frontendImage} alt="" style={{ height: "50px" }} />
+      <br />
+      <br />
+      <br />
+      <br />
+      <strong>App name: </strong> <span>{APP_NAME}</span>
     </>
   );
 }
