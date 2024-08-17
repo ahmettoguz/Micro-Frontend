e00c58013ad589e76e2cd6a7e64ea62ea6517da8
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-08-17 10:25:04 +0300

    template trial

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,57 @@
-// src/App.js
-import { useState } from "react";
-import { ThemeProvider } from "@mui/material/styles";
+import * as React from "react";
+import { PaletteMode } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
-import { Container } from "@mui/material";
-import Header from "./components/Header";
-import { getTheme } from "./utils/theme";
-import EmailForm from "./components/EmailForm";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import AutoAwesomeRoundedIcon from "@mui/icons-material/AutoAwesomeRounded";
+import AppAppBar from "./components/AppAppBar";
+import Hero from "./components/Hero";
+import LogoCollection from "./components/LogoCollection";
+import Highlights from "./components/Highlights";
+import Pricing from "./components/Pricing";
+import Features from "./components/Features";
+import Testimonials from "./components/Testimonials";
+import FAQ from "./components/FAQ";
+import Footer from "./components/Footer";
+// import getLPTheme from "./getLPTheme";
+import { useState } from "react";
 
-function App() {
-  const [themeMode, setThemeMode] = useState("light");
+export default function LandingPage() {
+  const [mode, setMode] = React.useState<PaletteMode>("light");
+  const [showCustomTheme, setShowCustomTheme] = useState(true);
+  // const LPtheme = createTheme(getLPTheme(mode));
+  const defaultTheme = createTheme({ palette: { mode } });
 
-  const theme = getTheme(themeMode);
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
+  };
 
-  const handleThemeToggle = () => {
-    setThemeMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+  const toggleCustomTheme = () => {
+    setShowCustomTheme((prev) => !prev);
   };
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={showCustomTheme ? defaultTheme : defaultTheme}>
       <CssBaseline />
-
-      <Header toggleTheme={handleThemeToggle} />
-
-      <Container sx={{ px: { xs: 1, sm: 2 } }}>
-        <EmailForm />
-      </Container>
+      <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+      {/* <Hero /> */}
+      {/* <Box sx={{ bgcolor: "background.default" }}>
+        <LogoCollection />
+        <Features />
+        <Divider />
+        <Testimonials />
+        <Divider />
+        <Highlights />
+        <Divider />
+        <Pricing />
+        <Divider />
+        <FAQ />
+        <Divider />
+        <Footer />
+      </Box> */}
     </ThemeProvider>
   );
 }
-
-export default App;
---
+++ src/App2.tsx
@@ -0,0 +1,32 @@
+
+import { useState } from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Container } from "@mui/material";
+// import Header from "./components/Header";
+import { getTheme } from "./utils/theme";
+import EmailForm from "./components/EmailForm";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const theme = getTheme(themeMode);
+
+  const handleThemeToggle = () => {
+    setThemeMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      {/* <Header toggleTheme={handleThemeToggle} /> */}
+
+      <Container sx={{ px: { xs: 1, sm: 2 } }}>
+        <EmailForm />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
