e9a5d364f6f0cccae9b0182e8c88344f980b1d22
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-08-17 17:44:41 +0300

    file change

--- src/App.tsx
+++ src/App.tsx
@@ -1,103 +1,118 @@
-import * as React from "react";
-import { PaletteMode } from "@mui/material";
+import { Container, PaletteMode } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import ToggleButton from "@mui/material/ToggleButton";
-import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import AutoAwesomeRoundedIcon from "@mui/icons-material/AutoAwesomeRounded";
-import AppAppBar from "./components/AppAppBar";
-import Hero from "./components/Hero";
-import LogoCollection from "./components/LogoCollection";
-import Highlights from "./components/Highlights";
-import Pricing from "./components/Pricing";
-import Features from "./components/Features";
-import Testimonials from "./components/Testimonials";
-import FAQ from "./components/FAQ";
-import Footer from "./components/Footer";
-import getLPTheme from "./utils/getLPTheme";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+import EmailForm from "./components/EmailForm";
+import XAppBar from "./components/sections/XAppBar";
 
-interface ToggleCustomThemeProps {
-  showCustomTheme: Boolean;
-  toggleCustomTheme: () => void;
-}
-
-function ToggleCustomTheme({
-  showCustomTheme,
-  toggleCustomTheme,
-}: ToggleCustomThemeProps) {
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "100dvw",
-        position: "fixed",
-        bottom: 24,
-      }}
-    >
-      <ToggleButtonGroup
-        color="primary"
-        exclusive
-        value={showCustomTheme}
-        onChange={toggleCustomTheme}
-        aria-label="Platform"
-        sx={{
-          backgroundColor: "background.default",
-          "& .Mui-selected": {
-            pointerEvents: "none",
-          },
-        }}
-      >
-        <ToggleButton value>
-          <AutoAwesomeRoundedIcon sx={{ fontSize: "20px", mr: 1 }} />
-          Custom theme
-        </ToggleButton>
-        <ToggleButton value={false}>Material Design 2</ToggleButton>
-      </ToggleButtonGroup>
-    </Box>
-  );
-}
-
-export default function App() {
-  const [mode, setMode] = React.useState<PaletteMode>("light");
-  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
-  const LPtheme = createTheme(getLPTheme(mode));
-  const defaultTheme = createTheme({ palette: { mode } });
+function App() {
+  const [themeMode, setThemeMode] = useState<PaletteMode>("light");
+  const theme = createTheme({ palette: { mode: themeMode } });
 
   const toggleColorMode = () => {
-    setMode((prev) => (prev === "dark" ? "light" : "dark"));
-  };
-
-  const toggleCustomTheme = () => {
-    setShowCustomTheme((prev) => !prev);
+    setThemeMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
   };
 
   return (
-    <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
-      <Hero />
-      <Box sx={{ bgcolor: "background.default" }}>
-        <LogoCollection />
-        <Features />
-        <Divider />
-        <Testimonials />
-        <Divider />
-        <Highlights />
-        <Divider />
-        <Pricing />
-        <Divider />
-        <FAQ />
-        <Divider />
-        <Footer />
-      </Box>
-      <ToggleCustomTheme
-        showCustomTheme={showCustomTheme}
-        toggleCustomTheme={toggleCustomTheme}
-      />
+
+      <XAppBar themeMode={themeMode} toggleColorMode={toggleColorMode} />
+
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          pt: 14,
+          pb: { xs: 8, sm: 12 },
+        }}
+      >
+        <EmailForm />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non
+          nobis magnam rem praesentium deserunt ut quae, placeat incidunt
+          molestiae repudiandae perferendis quos quod fugiat cumque expedita vel
+          sit totam. Ex soluta iusto necessitatibus molestias, numquam fugiat,
+          provident voluptatem facere perspiciatis nesciunt corporis quia alias
+          odio odit? Officia aliquid, blanditiis commodi consequatur, neque
+          iusto culpa quas, officiis unde dicta dolorum?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non
+          nobis magnam rem praesentium deserunt ut quae, placeat incidunt
+          molestiae repudiandae perferendis quos quod fugiat cumque expedita vel
+          sit totam. Ex soluta iusto necessitatibus molestias, numquam fugiat,
+          provident voluptatem facere perspiciatis nesciunt corporis quia alias
+          odio odit? Officia aliquid, blanditiis commodi consequatur, neque
+          iusto culpa quas, officiis unde dicta dolorum?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non
+          nobis magnam rem praesentium deserunt ut quae, placeat incidunt
+          molestiae repudiandae perferendis quos quod fugiat cumque expedita vel
+          sit totam. Ex soluta iusto necessitatibus molestias, numquam fugiat,
+          provident voluptatem facere perspiciatis nesciunt corporis quia alias
+          odio odit? Officia aliquid, blanditiis commodi consequatur, neque
+          iusto culpa quas, officiis unde dicta dolorum?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non
+          nobis magnam rem praesentium deserunt ut quae, placeat incidunt
+          molestiae repudiandae perferendis quos quod fugiat cumque expedita vel
+          sit totam. Ex soluta iusto necessitatibus molestias, numquam fugiat,
+          provident voluptatem facere perspiciatis nesciunt corporis quia alias
+          odio odit? Officia aliquid, blanditiis commodi consequatur, neque
+          iusto culpa quas, officiis unde dicta dolorum?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non
+          nobis magnam rem praesentium deserunt ut quae, placeat incidunt
+          molestiae repudiandae perferendis quos quod fugiat cumque expedita vel
+          sit totam. Ex soluta iusto necessitatibus molestias, numquam fugiat,
+          provident voluptatem facere perspiciatis nesciunt corporis quia alias
+          odio odit? Officia aliquid, blanditiis commodi consequatur, neque
+          iusto culpa quas, officiis unde dicta dolorum?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non
+          nobis magnam rem praesentium deserunt ut quae, placeat incidunt
+          molestiae repudiandae perferendis quos quod fugiat cumque expedita vel
+          sit totam. Ex soluta iusto necessitatibus molestias, numquam fugiat,
+          provident voluptatem facere perspiciatis nesciunt corporis quia alias
+          odio odit? Officia aliquid, blanditiis commodi consequatur, neque
+          iusto culpa quas, officiis unde dicta dolorum?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non
+          nobis magnam rem praesentium deserunt ut quae, placeat incidunt
+          molestiae repudiandae perferendis quos quod fugiat cumque expedita vel
+          sit totam. Ex soluta iusto necessitatibus molestias, numquam fugiat,
+          provident voluptatem facere perspiciatis nesciunt corporis quia alias
+          odio odit? Officia aliquid, blanditiis commodi consequatur, neque
+          iusto culpa quas, officiis unde dicta dolorum?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non
+          nobis magnam rem praesentium deserunt ut quae, placeat incidunt
+          molestiae repudiandae perferendis quos quod fugiat cumque expedita vel
+          sit totam. Ex soluta iusto necessitatibus molestias, numquam fugiat,
+          provident voluptatem facere perspiciatis nesciunt corporis quia alias
+          odio odit? Officia aliquid, blanditiis commodi consequatur, neque
+          iusto culpa quas, officiis unde dicta dolorum?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non
+          nobis magnam rem praesentium deserunt ut quae, placeat incidunt
+          molestiae repudiandae perferendis quos quod fugiat cumque expedita vel
+          sit totam. Ex soluta iusto necessitatibus molestias, numquam fugiat,
+          provident voluptatem facere perspiciatis nesciunt corporis quia alias
+          odio odit? Officia aliquid, blanditiis commodi consequatur, neque
+          iusto culpa quas, officiis unde dicta dolorum?
+        </p>
+      </Container>
     </ThemeProvider>
   );
 }
+
+export default App;
