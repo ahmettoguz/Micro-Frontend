f108e028cae69168ac580a1aac4ee8f423fe1e19
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-08-21 03:05:40 +0300

    -

--- src/components/core/sections/XAppBar.tsx
+++ src/components/core/sections/XAppBar.tsx
@@ -244,14 +244,14 @@ export default function XAppBar({
         <Container maxWidth="lg">
           <Toolbar
             variant="regular"
-            sx={{
+            sx={(theme) => ({
               display: "flex",
               alignItems: "center",
               justifyContent: "space-between",
               flexShrink: 0,
               borderRadius: "999px",
               bgcolor:
-                themeMode === "light"
+                theme.palette.mode === "light"
                   ? "rgba(255, 255, 255, 0.4)"
                   : "rgba(0, 0, 0, 0.4)",
               backdropFilter: "blur(24px)",
@@ -259,10 +259,10 @@ export default function XAppBar({
               border: "1px solid",
               borderColor: "divider",
               boxShadow:
-                themeMode === "light"
+                theme.palette.mode === "light"
                   ? `0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`
                   : "0 0 1px rgba(2, 31, 59, 0.7), 1px 1.5px 2px -1px rgba(2, 31, 59, 0.65), 4px 4px 12px -2.5px rgba(2, 31, 59, 0.65)",
-            }}
+            })}
           >
             {renderLeftItems()}
 
